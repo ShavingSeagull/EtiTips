f5befc96459ad220ab753e8e7424b2d5895a62dc
Shaving Seagull <scott.ecfx@gmail.com>
2019-06-27 00:12:08 +0100

    Changed the Main file to use onStart instead of onWindowFocus

--- app/src/main/java/com/example/etitips/MainActivity.kt
+++ app/src/main/java/com/example/etitips/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.etitips
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.view.animation.RotateAnimation
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,25 +16,13 @@ class MainActivity : AppCompatActivity() {
         val logo = findViewById<ImageView>(R.id.logo)
     }
 
-//    private fun logoAnim() {
-//        val pivotX: Float = logo.width / 2.toFloat()
-//        val pivotY: Float = logo.height / 2.toFloat()
-//        val rotate = RotateAnimation(0F,360F, pivotX, pivotY)
-//
-//        rotate.duration = 3000
-//        logo.startAnimation(rotate)
-//    }
-
+    override fun onStart() {
+        super.onStart()
+        logoAnim()
+    }
+    
     private fun logoAnim() {
         val logoBounce: Animation = AnimationUtils.loadAnimation(this, R.anim.logo_bounce)
         logo.startAnimation(logoBounce)
-    }
-
-    // TRY onStart() INSTEAD. OTHERWISE THE ANIMATION RUNS EVERY TIME THE WINDOW
-    // IS MINIMISED AND OPENED. GREAT FOR DEV, NOT GREAT FOR RELEASE
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        if (hasFocus) {
-            logoAnim()
-        }
     }
 }
