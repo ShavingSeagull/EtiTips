47baf3fe58c6765e02903ad4db4520237503c8d8
Shaving Seagull <scott.ecfx@gmail.com>
2019-06-28 23:51:05 +0100

    Finalised splash screen and button functionality. Minor updates to colors, toolbar etc

--- app/src/main/java/com/example/etitips/MainActivity.kt
+++ app/src/main/java/com/example/etitips/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.etitips
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Button
+//import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,15 +18,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val logo = findViewById<ImageView>(R.id.logo)
+        val splashButton = findViewById<Button>(R.id.splashButton)
     }
 
     override fun onStart() {
         super.onStart()
         logoAnim()
     }
-    
+
     private fun logoAnim() {
         val logoBounce: Animation = AnimationUtils.loadAnimation(this, R.anim.logo_bounce)
         logo.startAnimation(logoBounce)
+    }
+
+    fun enterApp(view: View) {
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
     }
 }
